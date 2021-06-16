6443b9b62d231f33f13a9e05a6194a26ec1091ef
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-06-15 21:36:54 -0300

    spes: props: Add support kernel idle timer
    * Indicates whether Scheduler's idle timer should support a display driver timeout in the kernel.
    * The value of set_idle_timer_ms should be shorter in time than the timeout duration in the kernel.
    
    * Reference 1: https://android.googlesource.com/platform/frameworks/native/+/master/services/surfaceflinger/sysprop/SurfaceFlingerProperties.sysprop
    
    * Reference 2: https://android.googlesource.com/device/google/redfin/+/refs/tags/android-11.0.0_r38/redfin/init.redfin.rc
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    
    [1] - This fixed screen flicker when using brightness at minimum, while using refresh rate dynamic

--- device.mk
+++ device.mk
@@ -249,6 +249,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.set_idle_timer_ms=500 \
     ro.surface_flinger.set_touch_timer_ms=800 \
+    ro.surface_flinger.support_kernel_idle_timer=true \
     ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
