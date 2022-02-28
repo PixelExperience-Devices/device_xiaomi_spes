67a5b2d9ab21f1544ec154b7050ad85c7fef37ca
Andrzej Perczak <linux@andrzejperczak.com>
2022-02-28 23:16:57 +0100

    spes: Disable radio redir_party_num feature
    Caused incoming caller number not being parsed correctly by Phone app on
    VoLTE and VoWiFi. No idea why Xiaomi enabled this ancient prop.
    
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -334,7 +334,6 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.radio.manual_nw_rej_ct=1 \
     persist.vendor.radio.mt_sms_ack=30 \
     persist.vendor.radio.process_sups_ind=1 \
-    persist.vendor.radio.redir_party_num=1 \
     persist.vendor.radio.report_codec=1 \
     persist.vendor.radio.snapshot_enabled=1 \
     persist.vendor.radio.snapshot_timer=5 \
