34b024702a4154ae30228044a7bced3ef7c97cfb
Alexander Koskovich <akoskovich@pm.me>
2022-03-22 19:56:23 -0300

    spes: bootctrl: Update to Boot Control HAL v1.2
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -137,9 +137,9 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
 
 # Camera
 PRODUCT_PACKAGES += \
