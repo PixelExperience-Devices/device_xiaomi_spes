9b8cff089035022b93e15c9b4202807b22884c97
chrisw444 <wandersonrodriguesf1@gmail.com>
2022-04-25 22:01:16 +0000

    spes: Switch to wfd component
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -404,7 +404,8 @@ DEVICE := spes
 TARGET_COMMON_QTI_COMPONENTS := \
     av \
     usb \
-    vibrator
+    vibrator \
+    wfd
 
 # QMI
 PRODUCT_PACKAGES += \
@@ -556,8 +557,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
-    libnl \
+    libdisplayconfig.system.qti \
+    libqdMetaData.system \
+    libdisplayconfig.vendor \
     libwfdaac_vendor
-
-#PRODUCT_BOOT_JARS += \
-    WfdCommon
