1d512c41bd6db6053291bd7a28d349b592661101
chrisw444 <wandersonrodriguesf1@gmail.com>
2022-04-25 19:20:29 +0000

    spes: Switch to vibrator component
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -402,6 +402,9 @@ BENGAL := bengal
 TARGET_BOARD_PLATFORM := $(BENGAL)
 DEVICE := spes
 
+TARGET_COMMON_QTI_COMPONENTS := \
+    vibrator
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
@@ -512,9 +515,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
 # Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
