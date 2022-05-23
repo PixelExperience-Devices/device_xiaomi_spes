4289eb7cb3337a538d8d76080691993a863700c5
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-05-22 20:20:19 -0500

    spes: Drop atrace service
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -52,10 +52,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
-# Atrace
-PRODUCT_PACKAGES += \
-    android.hardware.atrace@1.0-service
-
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService-Soong \
