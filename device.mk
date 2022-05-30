e66aa831311c6c5032455ebe566c01bd3e2a87b0
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-05-30 15:50:35 -0500

    spes: Switch to Xiaomi common hidl sensors
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -448,7 +448,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.xiaomi_bengal-multihal \
+    android.hardware.sensors@2.1-service.xiaomi-multihal \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
