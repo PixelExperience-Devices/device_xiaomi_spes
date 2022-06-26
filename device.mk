37748275de3885c811a7dc1e497ddaf2ad51b175
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-06-25 20:43:00 -0500

    spes: Update hardware.drm to 1.4
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -260,7 +260,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3.vendor \
+    android.hardware.drm@1.4.vendor \
     android.hardware.drm@1.4-service.clearkey
 
 PRODUCT_VENDOR_PROPERTIES += \
