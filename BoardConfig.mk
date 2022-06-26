a337566ee11176c6bcbd54ca8c6ec0d2b3f5f8b3
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-06-26 14:35:28 -0500

    spes: Introduce Firmware from V13.0.12.0.RGCMIXM
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -201,3 +201,6 @@ CONFIG_ACS := true
 
 # Inherit from the proprietary version
 include vendor/xiaomi/spes/BoardConfigVendor.mk
+
+# Inherit from the firmware
+include vendor/xiaomi/spes-firmware/BoardConfigVendor.mk
