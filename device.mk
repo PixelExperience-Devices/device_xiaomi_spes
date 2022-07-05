64839cea320423269dca265e96fa07ed3000738e
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-07-04 20:10:57 -0500

    spes: configs: Remove excluded-input-devices
    [1] - Use from source
    
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -595,10 +595,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
-# Vibrator
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
-
 # VNDK
 PRODUCT_EXTRA_VNDK_VERSIONS := 30
 
