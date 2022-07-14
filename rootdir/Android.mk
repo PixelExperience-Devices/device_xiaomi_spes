bc4715c8ea38188142b8cfd8807bf9096d4abf2d
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-07-14 00:38:31 +0000

    spes: rootdir: Move init spes and sper.perf to init/hw
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -56,7 +56,7 @@ LOCAL_MODULE       := init.spes.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.spes.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -64,7 +64,7 @@ LOCAL_MODULE       := init.spes.perf.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.spes.perf.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
