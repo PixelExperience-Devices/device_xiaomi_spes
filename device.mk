a47af679c8ea3a1ba2a6180386f35378451895de
chrisl <wandersonrodriguesf1@gmail.com>
2022-07-23 02:03:05 +0530

    spes: props: Removedexopt properties
    Signed-off-by: chrisl <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -13,12 +13,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
-PRODUCT_SYSTEM_PROPERTIES += \
-    pm.dexopt.install=speed-profile \
-    pm.dexopt.bg-dexopt=speed-profile \
-    pm.dexopt.boot=verify \
-    pm.dexopt.first-boot=quicken
-
 # Enable Virtual A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
