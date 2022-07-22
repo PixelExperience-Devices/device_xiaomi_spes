c66252734ddef4963b8de01f6250c8b65df0bd4a
chrisl <wandersonrodriguesf1@gmail.com>
2022-07-23 01:37:41 +0530

    spes: Fix definition to qcom/commmon FCM
    Signed-off-by: chrisl <wandersonrodriguesf1@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,10 +77,9 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_USES_METADATA_PARTITION := true
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/configs/hidl/xiaomi_framework_compatibility_matrix.xml \
-    $(COMMON_PATH)/vendor_framework_compatibility_matrix.xml
-
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/hidl/xiaomi_framework_compatibility_matrix.xml
+    
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/xiaomi_manifest.xml
