671ad94ff3aec203e9ce6d4c15c3472dfe87859e
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-09-07 13:14:58 +0000

    spes: init: Rework libinit
    [1] - Fix cts issues
    [2] - Based on aospa alioth
    [3] - Make everything simpler
    
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -104,8 +104,8 @@ ODM_MANIFEST_SKUS += k7tn
 ODM_MANIFEST_K7TN_FILES := $(DEVICE_PATH)/configs/hidl/manifest_k7tn.xml
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_spes
-TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_spes
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_spes
+TARGET_RECOVERY_DEVICE_MODULES := libinit_spes
 
 # Kernel
 BOARD_KERNEL_BASE        := 0x00000000
