0a103d8d06bd2825e5d39582ec9c3b15de360762
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-10-17 00:27:21 +0530

    spes: props: Import AOSP offsets properties
    [1] - https://github.com/PixelExperience/hardware_qcom-caf_sm8250-common_display/commit/79431bccb780f72dfc6653d9c6979edba095dbf3
    
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -213,6 +213,7 @@ PRODUCT_PACKAGES += \
     disable_configstore
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    debug.sf.frame_rate_multiple_threshold=90 \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
@@ -220,6 +221,15 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.set_touch_timer_ms=4000 \
     ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    debug.sf.use_phase_offsets_as_durations=1 \
+    debug.sf.late.sf.duration=10500000 \
+    debug.sf.late.app.duration=20500000 \
+    debug.sf.early.sf.duration=16000000 \
+    debug.sf.early.app.duration=16500000 \
+    debug.sf.earlyGl.sf.duration=13500000 \
+    debug.sf.earlyGl.app.duration=21000000
+    
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.sf.color_mode=9
 
