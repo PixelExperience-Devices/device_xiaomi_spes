1f5d5b6e45ddca411446755b9d2f6331592582d4
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-10-20 06:13:36 +0530

    spes: Remove duplicated definiton to compatibility_matrix
    [1] - https://github.com/PixelExperience-Devices/device_qcom_common/commit/778be7f626f297d8118610daca0ac6d1772fe588
    
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,7 +90,6 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/tp_gesture"
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/configs/hidl/xiaomi_framework_compatibility_matrix.xml
 
-DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/xiaomi_manifest.xml
 
