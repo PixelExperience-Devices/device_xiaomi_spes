c281ff15366529d3bbdb9450f6ed1e526409a18e
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-12-30 12:28:58 -0400

    spes: overlay: Import CarrierConfigOverlay from qcom/common
    [1] - LA.QSSI.13.0.r1-07400-qssi.0
    
    Change-Id: I18336c818e0f453742e7d158b9bfa422f6ea89c4

--- device.mk
+++ device.mk
@@ -413,6 +413,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
 
 # Overlays
 PRODUCT_PACKAGES += \
+    SpesCarrierConfigOverlay \
     SpesFrameworksOverlay \
     SpesSettingsOverlay \
     SpesSystemUIOverlay \
