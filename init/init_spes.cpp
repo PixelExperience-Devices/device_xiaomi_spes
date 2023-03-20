AOSPA/android_device_xiaomi_spes

spes: init: Fix product.mod_device property
Change-Id: I47b35d94b42f4a5e73305802317e59b794a379cd
Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

init/init_spes.cpp
@@ -60,7 +60,7 @@ void load_redmi_spes() {
     property_override("ro.product.manufacturer", "Xiaomi");
     property_override("ro.product.marketname", "Redmi Note 11");
     property_override("ro.product.model", "2201117TG");
-    property_override("ro.product.mod_device", "aospa_spes");
+    property_override("ro.product.mod_device", "spes_global");
     property_override("ro.product.name", "spes_global");
     property_override("vendor.usb.product_string", "Redmi Note 11");
 }
@@ -72,7 +72,7 @@ void load_redmi_spes_in() {
     property_override("ro.product.manufacturer", "Xiaomi");
     property_override("ro.product.marketname", "Redmi Note 11");
     property_override("ro.product.model", "2201117TI");
-    property_override("ro.product.mod_device", "aospa_spes_in");
+    property_override("ro.product.mod_device", "spes_in");
     property_override("ro.product.name", "spes_in");
     property_override("vendor.usb.product_string", "Redmi Note 11");
 }
@@ -84,7 +84,7 @@ void load_redmi_spesn() {
     property_override("ro.product.manufacturer", "Xiaomi");
     property_override("ro.product.marketname", "Redmi Note 11 NFC");
     property_override("ro.product.model", "2201117TY");
-    property_override("ro.product.mod_device", "aospa_spesn");
+    property_override("ro.product.mod_device", "spes_gobal");
     property_override("ro.product.name", "spesn");
     property_override("vendor.usb.product_string", "Redmi Note 11 NFC");
 }

PixelExperience-Devices/device_xiaomi_spes

init/init_spes.cpp
@@ -60,7 +60,7 @@ void load_redmi_spes() {
     property_override("ro.product.manufacturer", "Xiaomi");
     property_override("ro.product.marketname", "Redmi Note 11");
     property_override("ro.product.model", "2201117TG");
-    property_override("ro.product.mod_device", "aosp_spes");
+    property_override("ro.product.mod_device", "spes_global");
     property_override("ro.product.name", "spes_global");
     property_override("vendor.usb.product_string", "Redmi Note 11");
 }
@@ -72,7 +72,7 @@ void load_redmi_spes_in() {
     property_override("ro.product.manufacturer", "Xiaomi");
     property_override("ro.product.marketname", "Redmi Note 11");
     property_override("ro.product.model", "2201117TI");
-    property_override("ro.product.mod_device", "aosp_spes_in");
+    property_override("ro.product.mod_device", "spes_in");
     property_override("ro.product.name", "spes_in");
     property_override("vendor.usb.product_string", "Redmi Note 11");
 }
@@ -84,7 +84,7 @@ void load_redmi_spesn() {
     property_override("ro.product.manufacturer", "Xiaomi");
     property_override("ro.product.marketname", "Redmi Note 11 NFC");
     property_override("ro.product.model", "2201117TY");
-    property_override("ro.product.mod_device", "aosp_spesn");
+    property_override("ro.product.mod_device", "spesn_gobal");
     property_override("ro.product.name", "spesn");
     property_override("vendor.usb.product_string", "Redmi Note 11 NFC");
 }