af63b04e96a6d0be3c2ae9bcf1c69d70c1a96e7b
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-09-06 23:29:36 +0000

    spes: props: Set correct props for vrr
    Change-Id: I01874ba529574347981aae180708896118c076d3
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -57,7 +57,9 @@ PRODUCT_COPY_FILES += \
 
 # Display
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.set_touch_timer_ms=200 \
+    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.set_idle_timer_ms=500 \
+    ro.surface_flinger.set_touch_timer_ms=800 \
     ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
 PRODUCT_VENDOR_PROPERTIES += \
