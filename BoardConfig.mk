32bc1e75cd5fd24c4016400c25973f889d2fbe02
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-09-25 23:47:10 +0000

    spes: Adapt OSS kernel build to AOSP/PixelExperience
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,8 +30,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_spes
 
 # Kernel
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-KERNEL_DEFCONFIG := vendor/spes-perf_defconfig
-KERNEL_LLVM_SUPPORT := true
+TARGET_KERNEL_CONFIG := vendor/spes-perf_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6225
 
 # OTA assert
